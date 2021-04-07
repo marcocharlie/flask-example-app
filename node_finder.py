3ba6a261784ef5897255f70f37dd61d998ec9ebb
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-07 12:20:03 +0200

    Update node_finder.py

--- node_finder.py
+++ node_finder.py
@@ -1,17 +1,51 @@
-from data.utils import create_db_connection, read_query
+from data.utils import create_db_connection, read_query, format_query_results
+from os import getenv
 
 
 def find_nodes(node_id, language, search_keyword, page_num, page_size):
 
+    db_address = getenv("DB_ADDRESS")
+    db_user = getenv("DB_USER")
+    db_pwd = getenv("DB_PWD")
+    db_name = getenv("DB_NAME")
+
     # Connect to the Database
-    connection = create_db_connection("localhost", "root", "martina8", "nodes")
+    connection = create_db_connection(db_address, db_user, db_pwd, db_name)
 
-    q1 = """
+    primary_node_data = get_node(connection, node_id)
+    if len(primary_node_data) == 0:
+        raise Exception("Invalid node id provided")
+
+    left = primary_node_data[0]["iLeft"]
+    right = primary_node_data[0]["iRight"]
+
+    query = """
         SELECT *
-        FROM node_tree;
-        """
+        FROM node_tree
+        LEFT JOIN node_tree_names
+        ON node_tree.idNode = node_tree_names.idNode
+        WHERE (node_tree.iLeft > %s) AND (node_tree.iRight < %s) AND (node_tree_names.language = '%s');
+        """ % (left, right, language)
 
-    results = read_query(connection, q1)
+    field_names, results = read_query(connection, query)
 
-    for result in results:
-        print(result)
+    df = format_query_results(field_names, results)
+
+    connection.close()
+
+    return df
+
+
+# Return primary node data
+def get_node(connection, node_id):
+
+    query = """
+        SELECT *
+        FROM %s
+        WHERE idNode = %s;
+        """ % ("node_tree", node_id)
+
+    field_names, results = read_query(connection, query)
+    df = format_query_results(field_names, results)
+
+    return df.to_dict(orient='records')
