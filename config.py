08cb631618fb727c0d0b914be6367042401b7114
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-07 21:35:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,17 +2,10 @@ from os import getenv
 
 
 def connection_config():
-
-    db_host = getenv("DB_HOST")
-    db_port = getenv("DB_PORT")
-    db_user = getenv("DB_USER")
-    db_pwd = getenv("DB_PWD")
-    db_name = getenv("DB_NAME")
-
     return {
-        'host': db_host,
-        'port': db_port,
-        'user': db_user,
-        'passwd': db_pwd,
-        'db': db_name
+        'host': getenv("DB_HOST"),
+        'port': getenv("DB_PORT"),
+        'user': getenv("DB_USER"),
+        'passwd': getenv("DB_PWD"),
+        'db': getenv("DB_NAME")
     }
