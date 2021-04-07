caa1dc7c5cce08d4629f21360bb77c4505856c7c
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-07 16:29:49 +0200

    Update node_finder.py

--- node_finder.py
+++ node_finder.py
@@ -2,6 +2,7 @@ from data.utils import create_db_connection, read_query, format_query_results
 from os import getenv
 
 
+# Return nodes data from DB
 def find_nodes(node_id, language, search_keyword, page_num, page_size):
 
     db_address = getenv("DB_ADDRESS")
@@ -12,13 +13,16 @@ def find_nodes(node_id, language, search_keyword, page_num, page_size):
     # Connect to the Database
     connection = create_db_connection(db_address, db_user, db_pwd, db_name)
 
+    # Search for primary node data
     primary_node_data = get_node(connection, node_id)
     if len(primary_node_data) == 0:
         raise Exception("Invalid node id provided")
 
+    # Keep primary node data for children search
     left = primary_node_data[0]["iLeft"]
     right = primary_node_data[0]["iRight"]
 
+    # Query database
     query = """
         SELECT *
         FROM node_tree
@@ -27,13 +31,18 @@ def find_nodes(node_id, language, search_keyword, page_num, page_size):
         WHERE (node_tree.iLeft > %s) AND (node_tree.iRight < %s) AND (node_tree_names.language = '%s');
         """ % (left, right, language)
 
-    field_names, results = read_query(connection, query)
+    if search_keyword != None:
+        query = query.replace(";", "")
+        query = "%s AND (node_tree_names.nodeName = '%s');" % (
+            query, search_keyword)
 
+    # Format and enrich raw data
+    field_names, results = read_query(connection, query)
     df = format_query_results(field_names, results)
 
     connection.close()
 
-    return df
+    return df.to_dict(orient='records')
 
 
 # Return primary node data
