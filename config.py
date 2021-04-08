2ef42668137499ee51fc78602ae1982387e66a7f
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-08 11:40:15 +0200

    revert config

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+from os import getenv
+
+
 def connection_config():
     return {
-        'host': "localhost",
-        'port': 3306,
-        'user': "root",
-        'passwd': "mysql secret password",
-        'db': "nodes"
+        'host': getenv("DB_HOST"),
+        'port': getenv("DB_PORT"),
+        'user': getenv("DB_USER"),
+        'passwd': getenv("DB_PWD"),
+        'db': getenv("DB_NAME")
     }
