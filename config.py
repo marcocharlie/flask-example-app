7b83b41a201842f7b457096134ca2d41c8cf8145
marcocharlie <44462518+marcocharlie@users.noreply.github.com>
2021-04-08 12:35:04 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
-from os import getenv
+from dotenv import load_dotenv
+from os import environ
 
 
 def connection_config():
+    load_dotenv()
+
     return {
-        'host': getenv("DB_HOST"),
-        'port': getenv("DB_PORT"),
-        'user': getenv("DB_USER"),
-        'passwd': getenv("DB_PWD"),
-        'db': getenv("DB_NAME")
+        'host': environ.get("DB_HOST"),
+        'port': environ.get("DB_PORT"),
+        'user': environ.get("DB_USER"),
+        'passwd': environ.get("DB_PWD"),
+        'db': environ.get("DB_NAME")
     }
